672ba1e3bc874bfb86da0450ba69df43f31e3a75
Asmo343 <jan.glomb1@gmail.com>
2025-03-29 11:27:34 +0100

    Added Counter-Strike2

--- components/form/GameSelector.tsx
+++ components/form/GameSelector.tsx
@@ -22,6 +22,7 @@ const GameSelector: React.FC<GameSelectorProps> = ({ onGameChange }) => (
                     <SelectItem className='text-white focus:text-white hover:bg-dark-300 focus:bg-dark-300' value="mc">Minecraft</SelectItem>
                     <SelectItem className='text-white focus:text-white hover:bg-dark-300 focus:bg-dark-300' value="roblox">Roblox</SelectItem>
                     <SelectItem className='text-white focus:text-white hover:bg-dark-300 focus:bg-dark-300' value="valorant">Valorant</SelectItem>
+                    <SelectItem className='text-white focus:text-white hover:bg-dark-300 focus:bg-dark-300' value="cs2">Counter-Strike 2</SelectItem>
                 </SelectGroup>
             </SelectContent>
         </Select>
